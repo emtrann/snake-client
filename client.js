fe0dc6da6e85c418810a17f1795c796572734190
Michelle Tran <michelletraan@hotmail.com>
2020-09-23 19:24:18 +0000

    added snake move up and initials in play

--- client.js
+++ client.js
@@ -7,6 +7,9 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server!')
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
--- play.js
+++ play.js
@@ -1,9 +1,24 @@
 const net = require('net');
 const { connect } = require ('./client.js')
 
+const conn = net.createConnection({ 
+  host: 'localhost',
+  port: 50541
+});
+
 /**
  * Establishes connection with the game server
  */
+
+conn.on('connect', () => {
+  conn.write('Name: MT3');
+});
+
+// conn.on('connect', () => {
+//   setTimeout(() => {
+//   conn.write('Move: up');
+//   }, 3000);
+// });
 
 console.log('Connecting ...');
 connect();
