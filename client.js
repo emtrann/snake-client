fea08c29a95059a5802431e2c4a41488353bc7da
Michelle Tran <michelletraan@hotmail.com>
2020-09-23 21:06:45 +0000

    added constant file for IP/PORT

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 /**
  * Establishes connection with the game server
@@ -6,8 +7,8 @@ const net = require('net');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
+    host: IP,
+    port: PORT
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
